6c0dbf5339bcad422e73b65a19d7f15dc4eaba55
Mazen Mokhtar <mazenmokhtar.iq@gmail.com>
2025-09-16 05:07:42 +0300

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,7 @@
+/** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    // Production-only CSS optimization plugins
-    ...(process.env.NODE_ENV === 'production' && {
-      'cssnano': {
-        preset: ['default', {
-          discardComments: {
-            removeAll: true,
-          },
-          normalizeWhitespace: true,
-          minifySelectors: true,
-          minifyParams: true
-        }]
-      }
-    })
   },
-}
+};
